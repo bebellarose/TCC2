130c575626d2b451eb30f766310e7728c417f667
bebellarose <129982684+bebellarose@users.noreply.github.com>
2023-06-26 16:31:30 -0300

    Add files via upload

---
+++ TCC_2-main/TCC/js/sobre.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.Texto1, .Texto2 , .Texto3, .Texto4, .Texto5, .localh2', {duration: 1000,
+rotate: {x: 0, y:80, z:0}});
+
+sr.reveal('#footer_content', {duration: 3000,
+rotate: {x: 0, y:10, z:0}});
+
+sr.reveal('.hr', {duration: 2000,
+rotate: {x: 80, y:50, z:0}});
+
+sr.reveal('.propaganda', {duration: 2000,
+rotate: {x: 0, y:10, z:0}});
+
+sr.reveal('.wrapper', {duration: 2000,
+rotate: {x: 0, y:10, z:0}});
+
+
+
+
+
